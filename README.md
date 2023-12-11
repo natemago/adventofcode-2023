1a736d57c7e142536847f0cd8fa7e88f260ae7c5
Pavle Jonoski <pavle.jonoski@gmail.com>
2023-12-11 17:57:37 +0100

    Day 11, Cosmic Expansion :star: :collision: :dizzy:

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Solutions to [Advent Of Code puzzles 2023](https://adventofcode.com/2023)
 * [Day 8, Haunted Wasteland](day8-haunted-wasteland) :ghost: :ghost: :ghost:
 * [Day 9, Mirage Maintenance](day9-mirage-maintenance) :palm_tree: :pager: :floppy_disk:
 * [Day 10, Pipe Maze](day10-pipe-maze) :bathtub: :plunger: :shower:
+* [Day 11, Cosmic Expansion] :star: :collision: :dizzy:
--- day11-cosmic-expansion/solution.jl
+++ day11-cosmic-expansion/solution.jl
@@ -15,8 +15,46 @@ function read_input(fn)
 end
 
 
-function part1(galaxies)
-    println(galaxies)
+function part1(galaxies, expansion_factor=2)
+    galaxies = [g for g in galaxies]
+    galaxies = sort(galaxies)
+    prev = galaxies[1]
+    for i in range(2, length(galaxies))
+        curr = galaxies[i]
+        diff = curr[1] - prev[1]
+        if diff > 1
+            for j in range(i, length(galaxies))
+                galaxies[j] = (galaxies[j][1] + (diff-1)*(expansion_factor-1), galaxies[j][2])
+            end
+            curr = (curr[1] + (diff -1)*((expansion_factor-1)), curr[2])
+        end
+        prev = curr
+    end
+
+    galaxies = sort(galaxies, by=last)
+    prev = galaxies[1]
+    for i in range(2, length(galaxies))
+        curr = galaxies[i]
+        diff = curr[2] - prev[2]
+        if diff > 1
+            for j in range(i, length(galaxies))
+                galaxies[j] = (galaxies[j][1], galaxies[j][2] + (diff-1)*(expansion_factor-1))
+            end
+            curr = (curr[1], curr[2] + (diff -1)*(expansion_factor-1))
+        end
+        prev = curr
+    end
+
+    paths = []
+    for i in range(1, length(galaxies) - 1)
+        for j in range(i+1, length(galaxies))
+            ax, ay = galaxies[i]
+            bx, by = galaxies[j]
+            push!(paths, abs(ax-bx) + abs(ay - by))
+        end
+    end
+    return sum(paths)
 end
 
-println("Part 1: ", part1(read_input("input")))
+@time println("Part 1: ", part1(read_input("input")))
+@time println("Part 2: ", part1(read_input("input"), 1000000))
